3e77c75b70101c7473fb27c46452246f2c9ec798
Ravishek Kumar <ravishekprojects@gmail.com>
2025-06-24 21:14:35 +0530

    adding the code file

---
+++ background.js
@@ -0,0 +1,6 @@
+// background.js
+// Author: Ravishek Kumar
+// Author Github URI: https://github.com/Ravishek-Projects
+// Project Repository URI: https://github.com/Ravishek-Projects/Chrome-Extension
+// Description: Handles all the browser level activities (e.g. tab management, etc.)
+// License: MIT
